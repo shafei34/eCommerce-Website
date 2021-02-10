264da55a8ef81f92637a7f96b50d6c889a9d7a51
Abdulrahman Emad Eldein Alshafei <shafei@pop-os.localdomain>
2021-02-10 06:25:35 +0200

    stripe payments setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-// const stripe = require('stripe')('');
+const stripe = require("stripe")(
+  "sk_test_51HPvYWFaga2M2G5aWw6jDVi41yv7zpjt3NHwsMLVcreBalyupSW2XQmY70OLBwY0IM7JDGLTPdlw5pBITdIM0y6000yGs4vKw6"
+);
 
 const app = express();
 
@@ -11,28 +13,23 @@ app.use(
 );
 app.use(express.json());
 
-// app.post('/payments/create', async (req, res) => {
-//   try {
-//     const { amount, shipping } = req.body;
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       shipping,
-//       amount,
-//       currency: 'usd'
-//     });
+app.post("/payments/create", async (req, res) => {
+  try {
+    const { amount, shipping } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      shipping,
+      amount,
+      currency: "usd",
+    });
 
-//     res
-//       .status(200)
-//       .send(paymentIntent.client_secret);
-
-//   } catch (err) {
-//     res
-//       .status(500)
-//       .json({
-//         statusCode: 500,
-//         message: err.message
-//       });
-//   }
-// })
+    res.status(200).send(paymentIntent.client_secret);
+  } catch (err) {
+    res.status(500).json({
+      statusCode: 500,
+      message: err.message,
+    });
+  }
+});
 
 app.get("*", (req, res) => {
   res.status(404).send("404, Not Found. test ci/cd v2");
